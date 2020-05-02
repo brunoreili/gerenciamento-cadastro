03c26d9203739613b6449f52c09f47a46be6e181
Bruno Bispo <brunoreili@hotmail.com>
2020-05-02 14:53:36 -0300

    fix/correção na DTO para se adequar ao formato de dados requisitados pelo front

--- src/main/java/gerenciamento/dto/TelefoneListagemDto.java
+++ src/main/java/gerenciamento/dto/TelefoneListagemDto.java
@@ -11,31 +11,18 @@ public class TelefoneListagemDto {
 	
 	private Long id;
 	private String loginOperador;
-	private String ddd;
+	private Integer ddd;
 	private Integer numero;
-	private String tipo;
+	private Integer tipo;
 	private String dataCadastro;
 	
 	public TelefoneListagemDto(Telefone telefone) {
 		this.id = telefone.getId();
 		this.loginOperador = telefone.getLoginOperador();
-		this.ddd = telefone.getDdd();
+		this.ddd = Integer.parseInt(telefone.getDdd());
 		this.numero = telefone.getNumero();
-		this.tipo = this.formatarTipo(telefone.getTipo());
+		this.tipo = telefone.getTipo();
 		this.dataCadastro = this.formataData(telefone.getDataCadastro());
-	}
-	
-	private String formatarTipo(Integer tipo) {
-		switch(tipo){
-	        case 1:
-	            return "CELULAR";
-	        case 2:
-	        	return "FIXO";
-	        case 3:
-	        	return "COMERCIAL"; 
-	        default:
-	        	return "";
-	    }
 	}
 	
 	private String formataData(LocalDateTime data) {
@@ -54,7 +41,7 @@ public class TelefoneListagemDto {
 		return loginOperador;
 	}
 
-	public String getDdd() {
+	public Integer getDdd() {
 		return ddd;
 	}
 
@@ -62,7 +49,7 @@ public class TelefoneListagemDto {
 		return numero;
 	}
 
-	public String getTipo() {
+	public Integer getTipo() {
 		return tipo;
 	}
 
